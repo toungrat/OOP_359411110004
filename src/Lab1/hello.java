3fb1d69fdcfc90130279fb91249c49a0b038f394
toungrat <mingkam3301@gmail.com>
2019-11-28 11:18:22 +0700

    delete

--- src/Lab1/hello.java
+++ src/Lab1/hello.java
@@ -5,7 +5,6 @@ public class hello {
     public static void main(String[] args) {
 
        System.out.println("hollo world,I am LEK");
-       System.out.println("Information System");
 
 
     }//main
